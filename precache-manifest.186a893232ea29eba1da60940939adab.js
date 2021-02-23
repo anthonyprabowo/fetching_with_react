2c687b5ddfe0b865f1ba08e35726d4b04ec17389
Anthony Prabowo <anthonyprabowo@users.noreply.github.com>
2021-02-22 19:22:53 -0800

    Updates

---
+++ precache-manifest.186a893232ea29eba1da60940939adab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff44333fc7da274663fc",
+    "url": "/fetching_with_react/static/css/main.04ba0bde.chunk.css"
+  },
+  {
+    "revision": "ff44333fc7da274663fc",
+    "url": "/fetching_with_react/static/js/main.4524edec.chunk.js"
+  },
+  {
+    "revision": "daa9228c29adf7541f94",
+    "url": "/fetching_with_react/static/js/runtime~main.3b7aa14a.js"
+  },
+  {
+    "revision": "256c5eb2a5ecf4c63aa2",
+    "url": "/fetching_with_react/static/js/2.f5f89913.chunk.js"
+  },
+  {
+    "revision": "869aa542dad3370f4f1588f777fdc9b6",
+    "url": "/fetching_with_react/index.html"
+  }
+];
